a05081955b36ad4a4eef7531f26346f693eafb13
Victor Oliveira <victor.oliveira20@icloud.com>
2023-09-27 16:02:47 -0300

    Create project

---
+++ src/test/java/com/victoroliveira/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victoroliveira.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
